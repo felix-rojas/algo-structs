b1a1119abc2f5461e55392d9f001b0024d2c5c62
felix-rojas <a01201946@tec.mx>
2023-08-07 18:18:18 -0600

    first commit

---
+++ 1_Funciones_Iterativas_Recursivas/funciones.h
@@ -0,0 +1,19 @@
+/*
+ * funciones.h
+ *
+ *  Created on: 07/10/2020
+ *      Author: Félix Javier Rojas Gallardo
+ */
+
+#include <iostream>
+
+class Funciones
+{
+public:
+    int sumaRecursiva(int number)
+    {
+        
+    };
+    int sumaIterativa(int number){};
+    int sumaDirecta(int number){};
+};
