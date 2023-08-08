aeb842d56c9853cd8ee6f982ffafabc1bc2e8579
felix-rojas <a01201946@tec.mx>
2023-08-07 19:58:12 -0600

    1_Funciones_Iterativas_Recursivas done

--- 1_Funciones_Iterativas_Recursivas/funciones.h
+++ 1_Funciones_Iterativas_Recursivas/funciones.h
@@ -12,8 +12,26 @@ class Funciones
 public:
     int sumaRecursiva(int number)
     {
-        
+        if (number == 0)
+        {
+            return number;
+        } 
+        else 
+        {
+            return number + sumaRecursiva(number-1);
+        }
     };
-    int sumaIterativa(int number){};
-    int sumaDirecta(int number){};
+    int sumaIterativa(int number)
+    {
+        int res = 0;
+        for (int i = 0; i <= number; i++)
+        {
+            res += i;
+        }
+        return res;
+    };
+    int sumaDirecta(int number)
+    {
+        return static_cast<int>(number * (number + 1) * 0.5);
+    };
 };
