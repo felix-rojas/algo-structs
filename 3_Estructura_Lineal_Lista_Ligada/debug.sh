e16b610afc8009a3606f576e65875e0092e0ea8d
felix-rojas <a01201946@tec.mx>
2023-09-21 13:04:04 -0600

    silent output for mem check

--- 3_Estructura_Lineal_Lista_Ligada/debug.sh
+++ 3_Estructura_Lineal_Lista_Ligada/debug.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 g++ main.cpp -g -o a && valgrind --leak-check=full ./a &> tee memcheck_out.txt
+=======
+g++ main.cpp -g -o a && valgrind --leak-check=full ./a &> memcheck_out.txt
+>>>>>>> a92b162 (silent output for mem check)
