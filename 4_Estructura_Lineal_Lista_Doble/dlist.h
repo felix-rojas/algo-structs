ded363998325cf0e5b77d011a539dfa3dd40a8e4
felix-rojas <a01201946@tec.mx>
2023-09-27 10:38:53 -0600

    dll done, improve search

--- 4_Estructura_Lineal_Lista_Doble/dlist.h
+++ 4_Estructura_Lineal_Lista_Doble/dlist.h
@@ -12,32 +12,27 @@ public:
 };
 
 template <typename T> class DList {
-public:
+private:
   int elements;
-  void insertHead(T value);
-  /**
-   * @brief Auxiliary method that inserts a value at the end of the list
-   * @param value is any value to be inserted
-   * @see insertion(T value)
-   * @return void
-   */
-  void insertTail(T value);
+  DNode<T> *head;
+  DNode<T> *tail;
   /**
    * @brief Auxiliary method that deletes the first value of the list
    * @return value of the deleted node
    * @see deleteAt(unsigned int index)
    */
-  T deleteHead();
+  void deleteHead();
   /**
    * @brief Auxiliary method that deletes the last value of the list
    * @return value of the deleted node
    * @see deleteAt(unsigned int index)
    */
-  T deleteTail();
+  void deleteTail();
 
-  DNode<T> *head;
-  DNode<T> *tail;
-
+public:
+  DList();
+  DList(const DList<T> &);
+  ~DList();
   /**
    * @brief Inserts a value at the end of the list, similar to append or
    * push_back
@@ -45,13 +40,6 @@ public:
    * @return void
    */
   void insertion(T value);
-  /**
-   * @brief Inserts a T value at a specified index, starting from 0
-   * @param value is any value to be inserted
-   * @param index is the position to place the value
-   * @return void
-   */
-  void insertion(unsigned int index, T value);
   /**
    * @brief Search for value in the list, return index or -1 if not found
    * @param value to look for
@@ -64,13 +52,13 @@ public:
    * @param index position to update said value
    * @param value to change
    */
-  void update(unsigned int index, T value);
+  void update(int index, T value);
   /**
    * @brief Delete a node from the list and return its value
    * @param index of the node to remove
    * @return value of the deleted node
    */
-  T deleteAt(unsigned int index);
+  void deleteAt(int index);
   /**
    * @brief Makes a string using sstream in the specified format of the Single
    * Linked List
@@ -84,22 +72,25 @@ public:
 /**
  * @section Method definitions
  */
+template <class T>
+DList<T>::DList() : head(nullptr), tail(nullptr), elements(0) {}
 
-template <class T> std::string DList<T>::toString() {
-  std::stringstream aux;
-  DNode<T> *p;
+template <class T> DList<T>::~DList() {
+  DNode<T> *p, *q;
 
   p = head;
-  aux << "[";
-  while (p != 0) {
-    aux << p->value;
-    if (p->next != 0) {
-      aux << ", ";
-    }
-    p = p->next;
+  while (p != nullptr) {
+    q = p->next;
+    delete p;
+    p = q;
   }
-  aux << "]";
-  return aux.str();
+  head = nullptr;
+  tail = nullptr;
+  elements = 0;
+}
+
+template <class T> std::string DList<T>::toString() {
+  return toStringForward();
 }
 
 template <class T> std::string DList<T>::toStringForward() {
@@ -127,93 +118,26 @@ template <class T> std::string DList<T>::toStringBackward() {
   aux << "[";
   while (p != nullptr) {
     aux << p->value;
-    if (p->prev != nullptr) {
+    if (p->prev != nullptr)
       aux << ", ";
-    }
     p = p->prev;
   }
   aux << "]";
   return aux.str();
 }
 
-template <typename T> void DList<T>::insertHead(T value) {
-  // Create a temp Node
-  DNode<T> *node = new DNode<T>(value);
-
-  // New node points at head
-  node->next = head;
-  // if head exists, update prev pointer
-  if (head != nullptr)
-    head->prev = node;
-  // The new Node is the Head now
-  head = node;
-
-  // If the linked list is empty
-  // The Tail is also the Head
-  if (elements == 0)
-    tail = head;
-
-  // One element is added
-  elements++;
-}
-
-template <typename T> void DList<T>::insertTail(T value) {
-  // if empty, invoke insertHead()
-  if (elements == 0) {
-    insertHead(value);
-    return;
-  }
-
-  // Create a new Node
-  DNode<T> *node = new DNode<T>(value);
-  // same as insertHead, this node becomes tail
-  tail->next = node;
-  // prev pointer of new node points to tail
-  node->prev = tail;
-  tail = node;
-  elements++;
-}
-
 template <typename T> void DList<T>::insertion(T value) {
-  // Check if the index is out of bound
-  // list has zero elements
-  if (elements == 0) {
-    insertHead(value);
-    return;
+  DNode<T> *newNode = new DNode<T>(value);
+  if (!head) {
+    head = newNode;
+    tail = newNode;
+    elements++;
+  } else {
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+    elements++;
   }
-  // list has index elements
-  else if (elements != 0) {
-    insertTail(value);
-    return;
-  }
-
-  // Start to find previous node
-  // from the Head
-  DNode<T> *prev = head;
-
-  // Traverse elements until tail
-  for (int i = 0; i < elements; ++i) {
-    prev = prev->next;
-  }
-
-  // Create the next node which is
-  // the element after previous node
-  DNode<T> *next = prev->next;
-
-  // Create a new node
-  DNode<T> *node = new DNode<T>(value);
-
-  // Insert new node between
-  // the previous node and the next node
-  node->next = next;
-  // update prev pointer as well
-  node->prev = prev;
-  prev->next = node;
-  // update prev of next to node
-  next->prev = node;
-
-  // increase list size
-  elements++;
 }
 // push_back method for LinkedList
 
@@ -246,7 +170,7 @@ template <typename T> int DList<T>::search(T element) {
   return index;
 }
 
-template <typename T> void DList<T>::update(unsigned int index, T value) {
+template <typename T> void DList<T>::update(int index, T value) {
   if (index > elements || index < 0) {
     throw;
   }
@@ -262,10 +186,10 @@ template <typename T> void DList<T>::update(unsigned int index, T value) {
 }
 
 // deletes the first element
-template <typename T> T DList<T>::deleteHead() {
+template <typename T> void DList<T>::deleteHead() {
   // Do nothing if empty
   if (elements == 0)
-    return -1;
+    throw;
 
   // Save head to node
   DNode<T> *node = head;
@@ -285,10 +209,10 @@ template <typename T> T DList<T>::deleteHead() {
 
   // One element is removed
   elements--;
-  return value_copy;
+  return;
 }
 
-template <typename T> T DList<T>::deleteTail() {
+template <typename T> void DList<T>::deleteTail() {
   // one element? remove head
   if (elements == 1) {
     return deleteHead();
@@ -309,17 +233,17 @@ template <typename T> T DList<T>::deleteTail() {
 
   // One element is removed
   elements--;
-  return value_copy;
+  return;
 }
 
-template <typename T> T DList<T>::deleteAt(unsigned int index) {
+template <typename T> void DList<T>::deleteAt(int index) {
   // cant delete with no elements
   if (elements == 0)
-    return -1;
+    throw;
 
   // index is out of bound
   if (index < 0 || index >= elements)
-    return -1;
+    throw;
 
   // removing first element
   if (index == 0) {
@@ -335,7 +259,7 @@ template <typename T> T DList<T>::deleteAt(unsigned int index) {
 
   // Find the element before
   // disjointed from the first element (head)
-  for (int i = 0; i < index; i++) {
+  for (int i = 1; i < index; i++) {
     prev = prev->next;
   }
 
@@ -350,14 +274,13 @@ template <typename T> T DList<T>::deleteAt(unsigned int index) {
   // update the second link
   next->prev = prev;
 
-  T value_copy = node->value;
   // It's now safe to remove
   // the selected index element
   delete node;
 
   // One element is removed
   elements--;
-  return value_copy;
+  return ;
 }
 
 #endif // DLIST_H
