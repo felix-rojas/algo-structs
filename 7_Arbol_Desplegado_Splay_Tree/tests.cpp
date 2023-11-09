a1b15e0e5a9b3fd4668ceb7c46166be08f9308f5
felix-rojas <a01201946@tec.mx>
2023-11-09 09:53:46 -0600

    add tests for splay tree

---
+++ 7_Arbol_Desplegado_Splay_Tree/tests.cpp
@@ -0,0 +1,209 @@
+/*
+ * tests.cpp
+ *
+ *  Created on: 30/10/2020
+ *      Author: clase
+ */
+
+#include "splay.h"
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  string in_ans = "", pre_ans = "";
+
+  int test_times = 1;
+  bool found = 0;
+  cout << ("------------------------") << endl;
+  cout << ("--- Empty tree tests ---") << endl;
+  cout << ("------------------------") << endl;
+  // empty tree tests removal
+  for (int i = 0; i < test_times; i++) {
+    SplayTree<int> my_splay_test;
+
+    cout << ("Testing for non existing value removal: ") << endl;
+    cout << ("Removed value ") << i << endl;
+    my_splay_test.remove(i);
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+    cout << ("Testing for non existing value removal: ") << endl;
+    cout << ("Removed value ") << i << endl;
+    my_splay_test.remove(i);
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Testing for empty tree non existing value search: ") << endl;
+    cout << ("Value to find ") << i << endl;
+    my_splay_test.find(i + 1);
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+    cout << ("Testing for empty tree non existing value search: ") << endl;
+    cout << ("Value to find ") << i << endl;
+    my_splay_test.find(i + 1);
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+  }
+
+  cout << endl;
+
+  for (int i = 0; i < test_times; i++) {
+    SplayTree<int> my_splay_test;
+    int test_value = 15;
+    cout << ("-----------------------------") << endl;
+    cout << ("--- Single node tree test ---") << endl;
+    cout << ("-----------------------------") << endl;
+    cout << ("Adding a single node with value: ") << test_value << endl;
+    my_splay_test.add(test_value);
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Testing for non existing value: ") << 139 << endl;
+    found = my_splay_test.find(139);
+    cout << (!found ? "success\n" : "fail\n");
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Testing for single existing value: ") << test_value << endl;
+    found = my_splay_test.find(test_value);
+    cout << (found ? "success\n" : "fail\n");
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Testing for single existing value removal: ") << endl;
+    my_splay_test.remove(test_value);
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Testing find for value removal: ") << endl;
+    found = my_splay_test.find(test_value);
+    cout << (!found ? "success\n" : "fail\n");
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Testing find for deleted value: ") << test_value << endl;
+    found = my_splay_test.find(test_value);
+    cout << (!found ? "success\n" : "fail\n");
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Testing find for non-existent value: ") << 12983 << endl;
+    found = my_splay_test.find(12983);
+    cout << (!found ? "success\n" : "fail\n");
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Re-Adding a single node with value: ") << test_value << endl;
+    my_splay_test.add(test_value);
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+
+    cout << ("Re-Finding a single node with value: ") << test_value << endl;
+    found = my_splay_test.find(test_value);
+    cout << (found ? "success\n" : "fail\n");
+    cout << ("Tree status: \n");
+    my_splay_test.print();
+    cout << ("------------ \n");
+    cout << endl;
+  }
+
+  SplayTree<int> my_splay;
+  my_splay.add(15);
+
+  in_ans = "[15]";
+
+  cout << "\n"
+       << "1.- esperada " << in_ans << "\n programa " << my_splay.inorder()
+       << "\n";
+  cout << (!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+
+  my_splay.add(10);
+  my_splay.add(17);
+  my_splay.add(7);
+  my_splay.add(13);
+  my_splay.add(16);
+  my_splay.print();
+  in_ans = "[7 10 13 15 16 17]";
+  pre_ans = "[16 13 7 10 15 17]";
+
+  cout << "\n"
+       << "2.- inorder esperada " << in_ans << "\n programa "
+       << my_splay.inorder() << "\n";
+  cout << (!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << "\n"
+       << "2.- preorder esperada " << pre_ans << "\n programa "
+       << my_splay.preorder() << "\n";
+  cout << (!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+
+  found = my_splay.find(15);
+  my_splay.print();
+  in_ans = "[7 10 13 15 16 17]";
+  pre_ans = "[15 13 7 10 16 17]";
+
+  cout << "\n"
+       << "3.- inorder esperada " << in_ans << "\n programa "
+       << my_splay.inorder() << "\n";
+  cout << (!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << "\n"
+       << "3.- preorder esperada " << pre_ans << "\n programa "
+       << my_splay.preorder() << "\n";
+  cout << (!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+  cout << "\n"
+       << "3.- esperada " << 1 << " programa " << found << "\n";
+  cout << (found == 1 ? "success\n" : "fail\n");
+
+  found = my_splay.find(17);
+  my_splay.print();
+  in_ans = "[7 10 13 15 16 17]";
+  pre_ans = "[17 16 15 13 7 10]";
+  cout << "\n"
+       << "4.- inorder esperada " << in_ans << "\n programa "
+       << my_splay.inorder() << "\n";
+  cout << (!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << "\n"
+       << "4.- preorder esperada " << pre_ans << "\n programa "
+       << my_splay.preorder() << "\n";
+  cout << (!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+  cout << "\n"
+       << "4.- esperada " << 1 << " programa " << found << "\n";
+  cout << (found == 1 ? "success\n" : "fail\n");
+
+  my_splay.remove(13); // bottum up
+  my_splay.print();
+  in_ans = "[7 10 15 16 17]";
+  pre_ans = "[15 7 10 16 17]";
+  cout << "\n"
+       << "5.- inorder esperada " << in_ans << "\n programa "
+       << my_splay.inorder() << "\n";
+  cout << (!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << "\n"
+       << "5.- preorder esperada " << pre_ans << "\n programa "
+       << my_splay.preorder() << "\n";
+  cout << (!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+
+  my_splay.remove(16); // bottum up
+  my_splay.print();
+  in_ans = "[7 10 15 17]";
+  pre_ans = "[15 7 10 17]";
+  cout << "\n"
+       << "6.- inorder esperada " << in_ans << "\n programa "
+       << my_splay.inorder() << "\n";
+  cout << (!in_ans.compare(my_splay.inorder()) ? "success\n" : "fail\n");
+  cout << "\n"
+       << "6.- preorder esperada " << pre_ans << "\n programa "
+       << my_splay.preorder() << "\n";
+  cout << (!pre_ans.compare(my_splay.preorder()) ? "success\n" : "fail\n");
+}
